51238eba59e7b476d193078917a19470eed189af
Abdullah Atef <atef.66.ae@gmail.com>
2025-09-17 13:15:52 +0300

    edit some code structure

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+DATABASE_NAME = 'budget.db'
+
+category_keywords = {
+    'Food': ['food', 'restaurant', 'groceries', 'meal', 'طعام', 'أكل', 'غذاء', 'فطار'],
+    'Drinks': ['drink', 'water', 'coffee', 'beverage', 'juice', 'شرب', 'مشروب', 'قهوة', 'مياه', 'عصير'],
+    'Trans.': ['transport', 'trans', 'bus', 'taxi', 'uber', 'gas', 'car', 'مواصلات', 'توصيلة', 'سيارة', 'عربية'],
+    'Clothes': ['cloth', 'shopping', 'apparel', 'هدوم', 'ملابس', 'تيشيرت', 'كسوة'],
+    'Family': ['family', 'kids', 'school', 'عائلة', 'أسرة', 'اسرة', 'بيت'],
+    'Hygiene': ['hygiene', 'صحة', 'نظافة', 'تنظيف', 'برفيوم', 'عطر'],       
+    'Home': ['House', 'buildings', 'home', "جمعية", "جمعيه", 'الأثاث', 'الاثاث', 'المنزل', 'العزال'],
+    'Charity': ['handout', 'charity', "صدقة", "صدقه"],
+    'Courses': ['course', 'درس', 'كورس'],
+    'New Flat': ['الشقة', 'ايجار', 'إيجار'],
+    'Others': []
+}
